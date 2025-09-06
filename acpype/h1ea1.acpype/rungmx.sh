f17abbf2d92221d4e0a8096570827253565f1d79
pajadozach <pajadozach@gmail.com>
2025-09-06 20:16:49 +0800

    Mol Dynamics

---
+++ acpype/h1ea1.acpype/rungmx.sh
@@ -0,0 +1,6 @@
+
+echo 0 | gmx editconf -f h1ea1_GMX.gro -bt octahedron -d 10 -c -princ
+gmx grompp -f em.mdp -c out.gro -p h1ea1_GMX.top -o em.tpr -v
+gmx mdrun -ntmpi 1 -v -deffnm em
+gmx grompp -f md.mdp -c em.gro -p h1ea1_GMX.top -o md.tpr -r em.gro
+gmx mdrun -ntmpi 1 -v -deffnm md
